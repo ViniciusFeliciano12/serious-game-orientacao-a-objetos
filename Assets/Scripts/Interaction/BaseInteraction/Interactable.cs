3f267ce0b5465f7bcc6d0b2c9da1a30708659523
vinicius <vinicius@trendmobile.com.br>
2025-09-06 21:48:03 -0300

    inimigos
    melhorias, animações, refatoração, armas

---
+++ Assets/Scripts/Interaction/BaseInteraction/Interactable.cs
@@ -0,0 +1,54 @@
+namespace EJETAGame
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Interactable : MonoBehaviour, IInteractable
+    {
+        //Which button the user must press to initiate the Interaction;
+        public KeyCode interactionKey;
+        public ItemDatabase key;
+        void Start(){
+            
+        }
+
+        
+        public virtual void Interact()
+        {
+            
+        }
+
+        public virtual void OnInteractEnter()
+        {
+            if (TryGetComponent<Renderer>(out var rend))
+            {
+                Material highlightMat = new(Shader.Find("Universal Render Pipeline/Lit"));
+                highlightMat.SetColor("_BaseColor", new Color(1f, 1f, 1f, 0.3f)); // branco transparente
+
+                List<Material> mats = new(rend.materials)
+                {
+                    highlightMat
+                };
+
+                rend.materials = mats.ToArray();
+            }
+        }
+
+        public virtual void OnInteractExit()
+        {
+            InteractionText.instance.SetText("");
+            if (TryGetComponent<Renderer>(out var rend))
+            {
+                // Remove o último material (highlight)
+                List<Material> mats = new(rend.materials);
+                if (mats.Count > 1)
+                {
+                    mats.RemoveAt(mats.Count - 1);
+                    rend.materials = mats.ToArray();
+                }
+            }
+        }
+    }
+
+}
